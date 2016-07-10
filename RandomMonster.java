ba0eb02d5082f7c934d32d99a8507efc98a3d1c9
G2 <acguy27@icloud.com>
2016-07-11 02:12:54 +0900

    Update RandomMonster.java

--- RandomMonster.java
+++ RandomMonster.java
@@ -4,10 +4,15 @@ public class RandomMonster{
   void run(String[] args){
     Map<String, List<String>> characterMap = new HashMap<String, List<String>>();
     characterMap=this.createMap();
+    List<String> abilityList = new ArrayList<String>();
+    
     System.out.println("***モンスト自動編成マシン***");
     System.out.println("[挑戦するクエストのアビリティを教えてね]");
+    System.out.println("使用できるアビリティ");
+    System.out.println("重力、壁、マイン、ワープ");
+    
       for(String ability:args){
-      
+        abilityList.add(ability);
       }
   }
   Map<String,List<String>> createMap(){
