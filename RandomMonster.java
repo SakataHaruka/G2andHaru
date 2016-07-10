bf10a4c91e09a19d30087f019a023a092c7f7fd6
G2 <acguy27@icloud.com>
2016-07-11 01:57:10 +0900

    Update RandomMonster.java

--- RandomMonster.java
+++ RandomMonster.java
@@ -1,21 +1,23 @@
 import java.util.*;
 import java.io.*;
 public class RandomMonster{
-  void run(String[] arg){
-  Map<String, List<String>> characterMap = new HashMap<String, List<String>>();
-  characterMap=this.createMap();
-  System.out.println("***モンスト自動編成マシン***");
-  System.out.println("[挑戦するクエストのアビリティを教えてね]");
-  
+  void run(String[] args){
+    Map<String, List<String>> characterMap = new HashMap<String, List<String>>();
+    characterMap=this.createMap();
+    System.out.println("***モンスト自動編成マシン***");
+    System.out.println("[挑戦するクエストのアビリティを教えてね]");
+      for(String ability:args){
+      
+      }
   }
   Map<String,List<String>> createMap(){
-  Map<String, List<String>> characterMap = new HashMap<String, List<String>>();
-  List<String> AttributeAndAbility=new ArrayList<String>();
-  characterMap.put();
-return characterMap;
+    Map<String, List<String>> characterMap = new HashMap<String, List<String>>();
+    List<String> AttributeAndAbility=new ArrayList<String>();
+    characterMap.put();
+    return characterMap;
   }
   public static void main(String[] args){
-  RandomMonster RM=new RandomMonster();
-  RM.run(arg);
+    RandomMonster RM=new RandomMonster();
+    RM.run(args);
   }
 }
