ca3b5ae9ac84b73bbf178e4ec3ffa7fd2e7bf155
SakataHaruka <g1544647@gmail.com>
2016-07-11 01:34:34 +0900

    Update RandomMonster.java

--- RandomMonster.java
+++ RandomMonster.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class RandomMonster{
   void run(String[] arg){
-  Map<Book, List<History>> historyMap = new HashMap<Book, List<History>>();
+  Map<String, List<String>> characterMap = new HashMap<String, List<String>>();
   
   }
   public static void main(String[] args){
