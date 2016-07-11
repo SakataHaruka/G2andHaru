f35b3a631c49ef044d8c100da5d2ca6166f39b7b
G2 <acguy27@icloud.com>
2016-07-11 13:49:27 +0900

    Update RandomMonster.java

--- RandomMonster.java
+++ RandomMonster.java
@@ -5,13 +5,14 @@ public class RandomMonster{
     Map<String, List<String>> characterMap = new HashMap<String, List<String>>();
     characterMap=this.createMap();
     List<String> abilityList = new ArrayList<String>();
+    List<String> member=new ArrayList<String>();
     
     System.out.println("***モンスト自動編成マシン***");
     System.out.println("[挑戦するクエストのアビリティを教えてね]");
     System.out.println("使用できるアビリティ");
     System.out.println("重力、壁、マイン、ワープ");
     
-    Integer i=1;
+    Integer i=0;
     String candidate
       for(String ability:args){
         if(ability==Mapkey){//書き方がわからない
@@ -19,10 +20,19 @@ public class RandomMonster{
         }
         i+=1;
       }
+      Integer count=0;
       for(String target:candidate){
         if(Object.equals(target,candidate)){
-        
+        count+=1;
         }
+        if(count==i){
+          //リスト型に渡す
+          member.add(target);
+        }
+      }
+      System.out.println("そのクエストに対応したキャラは")
+      for(String printmember:member){
+      System.out.println(printmember);
       }
   }
   Map<String,List<String>> createMap(){
